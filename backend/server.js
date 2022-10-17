f1cbfd010145fd36ac33ec51106d3e36bcc8d5ae
gautampraveen <gautampraveen351@gmail.com>
2022-10-17 15:07:43 +0530

    major changes

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,36 @@ connectDb();
 PORT = process.env.PORT || 8001;
 
 // app.use statements
-app.listen(PORT, () => {
+const socketserver = app.listen(PORT, () => {
   console.log("running on " + PORT);
 });
+
+// socket initialization
+const io = require("socket.io")(socketserver, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  // socket joining room
+  socket.on("setup", (email) => {
+    socket.join(email);
+    console.log("admin joined " + email);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log(room + "joined the room");
+  });
+  socket.on("send message", (usermessage) => {
+    console.log(usermessage);
+    socket.in(usermessage.to).emit("new message", usermessage);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+// socket initialization ends
